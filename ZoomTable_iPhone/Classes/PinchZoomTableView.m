227f61a91595e4e4f467670aee2b2157decb4cf3
sachithkadamba <sachithkadamba@gmail.com>
2011-02-07 17:28:42 +0530

    changed the license

--- ZoomTable_iPhone/Classes/PinchZoomTableView.m
+++ ZoomTable_iPhone/Classes/PinchZoomTableView.m
@@ -1,15 +1,5 @@
-
 //  PinchZoomTableView.m
-
 //  Created by Sachith on 14/01/11.
-//  Copyright 2011 Sachith Kadamba All rights reserved.
-//  Permission is given to use this source code file without charge in any
-//  project, commercial or otherwise, entirely at your risk, with the condition
-//  that any redistribution (in part or whole) of source code must retain
-//  this copyright and permission notice. Attribution in compiled projects is
-//  appreciated but not required.
-//  For any Queries or suggestion contact me :sachithkadamba@gmail.com
-
 
 #import "PinchZoomTableView.h"
 #import <QuartzCore/QuartzCore.h>
